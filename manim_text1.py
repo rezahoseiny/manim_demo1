0defd8eba7c30660d053662f53c8ee44d9fbf81c
reza <10123737+rezahoseiny@users.noreply.github.com>
2025-01-02 09:58:42 +1100

    Update manim_text1.py

--- manim_text1.py
+++ manim_text1.py
@@ -1,1 +1,7 @@
+from manim import *
 
+class HelloWorld(Scene):
+    def construct(self):
+        print("Hello from Binder!")
+        text = Text("Hello world", font_size=144)
+        self.add(text)
